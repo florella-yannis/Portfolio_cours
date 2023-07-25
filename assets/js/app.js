3a492f051bc293b79d0c5aa1335dfd8d3d28ccb4
florella-yannis <florellayannis@gmail.com>
2023-07-25 15:44:58 +0200

    toggle mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,25 @@
 
-/*let sun = document.querySelector('#sun');
-let moon = document.querySelector('#moon');
-let turn = document.querySelector('.turn')
+const toggle = document.getElementById('toggleDark');
 
-turn.addEventListener('click',function(){
+toggle.addEventListener('click', function () {
+    const html = document.querySelector('html');
+    if (html.classList.contains('light')) {
+        html.classList.remove('light');
+    } else {
+        html.classList.add('light');
+    }
+});
 
-    sun.classList.toggle('moon');
-    moon.classList.toggle('sun');
+toggle.addEventListener('click', function () {
+    if (this.classList.contains('fa-sun')) {
+        this.classList.replace('fa-sun', 'fa-moon');
+        // Ajoutez ici le code pour basculer vers le thème sombre si nécessaire
+    } else {
+        this.classList.replace('fa-moon', 'fa-sun');
+        // Ajoutez ici le code pour basculer vers le thème clair si nécessaire
+    }
+});
 
-
-})*/
 // a refaire 
 const menuHamburger = document.querySelector(".fa-bars")
 const navLinks = document.querySelector("#nav-links")
